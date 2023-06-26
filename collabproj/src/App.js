9f0b60f991f8f67b0de6aec45ccf7fa2066947da
Debopriyo6 <debopriyoray662001@gmail.com>
2023-06-26 22:59:56 +0530

    Navbar

--- collabproj/src/App.js
+++ collabproj/src/App.js
@@ -1,10 +1,16 @@
 import Navbar from "./Components/Navbar";
-
+import { globalcontext } from "./Components/GlobalContext";
+import { useState } from "react";
 
 function App() {
+
+  const[login,setLogin]=useState(false)
   return (
    <div>
+    <globalcontext.Provider value={{login,setLogin}}>
+
     <Navbar/>
+    </globalcontext.Provider>
     </div>
   );
 }
