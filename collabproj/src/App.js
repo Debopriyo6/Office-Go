a52f61aa8357940f72a530fe3bdc72dfe93526a0
Debopriyo6 <debopriyoray662001@gmail.com>
2023-06-30 23:28:43 +0530

    login form design

--- collabproj/src/App.js
+++ collabproj/src/App.js
@@ -1,6 +1,8 @@
 import Navbar from "./Components/Navbar";
 import { globalcontext } from "./Components/GlobalContext";
 import { useState } from "react";
+import { Route, Routes } from "react-router";
+import Login from "./Components/Login&Register/Login";
 
 function App() {
 
@@ -10,6 +12,10 @@ function App() {
     <globalcontext.Provider value={{login,setLogin}}>
 
     <Navbar/>
+    <Routes>
+      <Route path="/login" element={<Login/>}/>
+       
+    </Routes>
     </globalcontext.Provider>
     </div>
   );
--- collabproj/src/Components/Navbar.js
+++ collabproj/src/Components/Navbar.js
@@ -1,28 +1,30 @@
 import "./Navbar.css";
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-const Container=styled.div`
-color:blue;
-
-`
-
+const Container = styled.div`
+  color: blue;
+`;
 
 const Navbar = () => {
   return (
     <div>
-          <Container>
-           <h4>Shuttl</h4>
-          </Container>
-      <div className='sidebar'>
-       <Link className="active" to="/">My Pass</Link>
-    <Link to="/">My Rides</Link>
-    <Link to="/">Wallets</Link>
-    <Link to="/">Routes</Link>
-  
+      <Container>
+        <h4>Shuttl</h4>
+      </Container>
+      
+      <div className="sidebar">
+        <Link className="active" to="/login">
+          Login
+        </Link>
+        <Link to="/">My Pass</Link>
+        <Link to="/">My Rides</Link>
+        <Link to="/">Wallets</Link>
+        <Link to="/">Routes</Link>
       </div>
-      </div>
-  )
-}
+    </div>
+    
+  );
+};
 
 export default Navbar;
