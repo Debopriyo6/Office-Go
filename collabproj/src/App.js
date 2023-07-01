a28d14821cc301dc2e39b8f25ae45796f308c1f6
Akash Panday <akashpanday11@gmail.com>
2023-07-01 12:41:12 +0530

    restructured files

--- collabproj/src/App.js
+++ collabproj/src/App.js
@@ -1,23 +1,18 @@
-import Navbar from "./Components/Navbar";
-import { globalcontext } from "./Components/GlobalContext";
+import Navbar from "./Components/Navbar/Navbar";
+import { globalcontext } from "./GlobalContext";
 import { useState } from "react";
 import { Route, Routes } from "react-router";
 import Login from "./Components/Login&Register/Login";
 
 function App() {
-
-  const[login,setLogin]=useState(false)
+  const [login, setLogin] = useState(false);
   return (
-   <div>
-    <globalcontext.Provider value={{login,setLogin}}>
-
-    <Navbar/>
-    <Routes>
-      <Route path="/login" element={<Login/>}/>
-       
-    </Routes>
+    <globalcontext.Provider value={{ login, setLogin }}>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </globalcontext.Provider>
-    </div>
   );
 }
 
