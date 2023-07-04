2193bde57f65e41ad926d679de442efd0814d920
Debopriyo6 <debopriyoray662001@gmail.com>
2023-07-04 22:43:06 +0530

    validation & local storage

--- collabproj/src/App.js
+++ collabproj/src/App.js
@@ -3,12 +3,14 @@ import { globalcontext } from "./GlobalContext";
 import { useState } from "react";
 import { Route, Routes } from "react-router";
 import Login from "./Components/Login&Register/Login";
+import { useSearchParams } from "react-router-dom";
 
 function App() {
   const [login, setLogin] = useState(false);
+  const[store,setStore]=useState([]);
 
   return (
-    <globalcontext.Provider value={{ login, setLogin }}>
+    <globalcontext.Provider value={{ login, setLogin,store,setStore }}>
       <Navbar />
       <Routes>
         <Route path="/auth" element={<Login />} />
